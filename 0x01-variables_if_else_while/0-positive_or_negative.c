0a8c7771afc69cbd9136fda8c59e1571991144c8
Hika7 <hikadaka1@gmail.com>
2022-06-03 13:39:31 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,7 +16,7 @@ int main(void)
 	
 	if (n > 0)
 	{
-	    printf("%d is positive\n", n);	
+	    printf("%d is positive\n", n);
 	}
         else if (n == 0)
 	{
@@ -28,4 +28,4 @@ int main(void)
         }
         
         return (0);
-}	
+}
