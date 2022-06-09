aab8f13b9ba545421acbb4862139782f5bacaebd
Hika7 <hikadaka1@gmail.com>
2022-06-09 02:42:44 -0700

    declare

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,8 +14,7 @@ if (n >= 0 && n <= 15)
 for (num = 0; num <= n; num++)
 {
 _putchar('0');
-for (mult = 1; mult <= n; mult++)
-{
+
 for (mult = 1; mult <= n; mult++)
 {
 _putchar(',');
@@ -40,6 +39,5 @@ _putchar((prod / 10) + '0');
 _putchar((prod % 10) + '0');
 }
 _putchar('\n');
-}
 }
 }
